5169a121ea01a9d3faae81edf2181c8f7d2b2fe2
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-12-03 21:53:36 +0530

    API key removed

--- src/main/kotlin/Remote/HttpRoutes.kt
+++ src/main/kotlin/Remote/HttpRoutes.kt
@@ -3,7 +3,7 @@ package Remote
 object HttpRoutes {
 
     private const val BASE_URL = "https://api.weatherapi.com/v1"
-    const val API_KEY = "aed75529e120456d98661744212709"
+    const val API_KEY = ""
 
     const val CURRENT = "$BASE_URL/current.json?"
     const val FORECAST = "$BASE_URL"
